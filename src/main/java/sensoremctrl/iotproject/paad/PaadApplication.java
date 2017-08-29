1004f3837da570c235ead05476daf288ac0102da
Patricio <patricio89morales5@hotmail.com>
2017-08-29 11:18:06 +0200

    Initial commit

---
+++ src/main/java/sensoremctrl/iotproject/paad/PaadApplication.java
@@ -0,0 +1,12 @@
+package sensoremctrl.iotproject.paad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaadApplication.class, args);
+	}
+}
