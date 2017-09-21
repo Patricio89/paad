23e0f22d5330a4348940c8355e7e9d1658f61c6e
Patricio <patricio89morales5@hotmail.com>
2017-09-21 11:23:22 +0200

    minor fixes

--- src/main/java/sensoremctrl/iotproject/paad/ProcessManagement/DataProcessor.java
+++ src/main/java/sensoremctrl/iotproject/paad/ProcessManagement/DataProcessor.java
@@ -2,4 +2,10 @@ package sensoremctrl.iotproject.paad.ProcessManagement;
 
 public class DataProcessor {
 
+    private DataStorage dataStorage;
+
+    public void checkContent(){
+        System.out.println(dataStorage.getDataSamples());
+    }
+
 }
