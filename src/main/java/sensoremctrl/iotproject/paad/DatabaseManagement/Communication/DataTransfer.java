e4447a8c9206995585740877cd4eee0e89a479e3
Adrian <adde15w@hotmail.com>
2017-09-29 09:27:21 +0200

    Application updated and refactored

--- src/main/java/sensoremctrl/iotproject/paad/DatabaseManagement/Communication/DataTransfer.java
+++ src/main/java/sensoremctrl/iotproject/paad/DatabaseManagement/Communication/DataTransfer.java
@@ -1,71 +1,71 @@
-//package sensoremctrl.iotproject.paad.DatabaseManagement.Communication;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import sensoremctrl.iotproject.paad.DataManagement.DataProcessor;
-//import sensoremctrl.iotproject.paad.DataManagement.DataValue;
-//import sensoremctrl.iotproject.paad.DataManagement.LocalDataStorage;
-//import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.DateAndTimeLog;
-//import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.HumidityLog;
-//import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.TemperatureLog;
-//
-//import java.io.Serializable;
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
-//
-//public class DataTransfer implements Serializable{
-//
-//    @Autowired
-//    LocalDataStorage dataStorage;
-//
-//    @Autowired
-//    DataProcessor processor;
-//
-//    private List<DataValue> dataStorages = dataStorage.getDataValueList();
-//
-//
-//    private List<TemperatureLog> temperatureLogList = new ArrayList<>();
-//    private List<HumidityLog> humidityLogList = new ArrayList<>();
-//    private List<DateAndTimeLog> dateAndTimeLogList = new ArrayList<>();
-//
-//
-//    private void storeTemperature(){
-//        for (int i = 0; i < temperatureLogList.size(); i++){
-//            temperatureLogList.add(new TemperatureLog(dataStorages.get(i).getTemperature()));
-//        }
-//    }
-//
-//    private void storeHumidity(){
-//        for (int i = 0; i < humidityLogList.size(); i++){
-//            humidityLogList.add(new HumidityLog(dataStorages.get(i).getHumidity()));
-//        }
-//    }
-//
-//    //Probably error due to double parsing. --check LocalDataStorage--
-//    private void storeDateAndTime(){
-//        Date stringToDate = processor.convertStringToDate(dataStorages);
-//        for (int i = 0; i < dateAndTimeLogList.size(); i++){
-//
-//            dateAndTimeLogList.add(new DateAndTimeLog(stringToDate));
-//
-//        }
-//    }
-//
-//
-//    public List<TemperatureLog> getTemperatureList(){
-//        storeTemperature();
-//        return this.temperatureLogList;
-//    }
-//
-//    public List<HumidityLog> getHumidityList(){
-//        storeHumidity();
-//        return this.humidityLogList;
-//    }
-//
-//
-//    public List<DateAndTimeLog> getDateAndTimeList(){
-//        storeDateAndTime();
-//        return this.dateAndTimeLogList;
-//    }
-//
-//}
+package sensoremctrl.iotproject.paad.DatabaseManagement.Communication;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import sensoremctrl.iotproject.paad.DataManagement.DataProcessor;
+import sensoremctrl.iotproject.paad.DataManagement.DataValue;
+import sensoremctrl.iotproject.paad.DataManagement.LocalDataStorage;
+import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.DateAndTimeLog;
+import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.HumidityLog;
+import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.TemperatureLog;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class DataTransfer {
+
+    @Autowired
+    LocalDataStorage localDataStorage;
+
+    @Autowired
+    DataProcessor processor;
+
+    private List<DataValue> csvDataValues;
+
+    @Bean
+    public List<TemperatureLog> storeTemperatureLog() {
+        csvDataValues = localDataStorage.getDataValueList();
+        List<TemperatureLog> temperatureLogList = new ArrayList<>();
+        TemperatureLog temperatureLog = new TemperatureLog();
+        int temperature;
+        for (int i = 0; i < csvDataValues.size(); i++) {
+            temperature = csvDataValues.get(i).getTemperature();
+            temperatureLog.setTemperature(temperature);
+            temperatureLogList.add(new TemperatureLog(temperatureLog.getTemperature()));
+        }
+        return temperatureLogList;
+    }
+
+    @Bean
+    public List<HumidityLog> storeHumidityLog() {
+        csvDataValues = localDataStorage.getDataValueList();
+        List<HumidityLog> humidityLogList = new ArrayList<>();
+        HumidityLog humidityLog = new HumidityLog();
+        int humidity;
+        for (int i = 0; i < csvDataValues.size(); i++) {
+            humidity = csvDataValues.get(i).getHumidity();
+            humidityLog.setHumidity(humidity);
+            humidityLogList.add(new HumidityLog(humidityLog.getHumidity()));
+        }
+        return humidityLogList;
+    }
+
+    @Bean
+    public List<DateAndTimeLog> storeTimeStamp() {
+        csvDataValues = localDataStorage.getDataValueList();
+        List<DateAndTimeLog> dateAndTimeLogList = new ArrayList<>();
+        DateAndTimeLog dateAndTimeLog = new DateAndTimeLog();
+        Date timeStamp;
+        for (int i = 0; i < csvDataValues.size(); i++) {
+            timeStamp = processor.convertStringToDate(csvDataValues.get(i).getDateAndTime());
+            dateAndTimeLog.setTimeStamp(timeStamp);
+            dateAndTimeLogList.add(new DateAndTimeLog(dateAndTimeLog.getTimeStamp()));
+        }
+        return dateAndTimeLogList;
+    }
+
+}
--- src/main/java/sensoremctrl/iotproject/paad/DatabaseManagement/Communication/DatabaseLink.java
+++ src/main/java/sensoremctrl/iotproject/paad/DatabaseManagement/Communication/DatabaseLink.java
@@ -1,34 +1,32 @@
-//package sensoremctrl.iotproject.paad.DatabaseManagement.Communication;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import sensoremctrl.iotproject.paad.Model.DateAndTimeRepository;
-//import sensoremctrl.iotproject.paad.Model.HumidityRepository;
-//import sensoremctrl.iotproject.paad.Model.TemperatureRepository;
-//
-//
-//public class DatabaseLink {
-//
-//    @Autowired
-//    DataTransfer transmitter;
-//
-//    @Autowired
-//    TemperatureRepository temperatureRepository;
-//
-//    @Autowired
-//    HumidityRepository humidityRepository;
-//
-//    @Autowired
-//    DateAndTimeRepository dateAndTimeRepository;
-//
-//
-//    public void sendDataToDatabase(){
-//        temperatureRepository.save(transmitter.getTemperatureList());
-//        humidityRepository.save(transmitter.getHumidityList());
-//        dateAndTimeRepository.save(transmitter.getDateAndTimeList());
-//    }
-//
-//
-//    public void getDataFromDatabase(){
-//
-//    }
-//}
+package sensoremctrl.iotproject.paad.DatabaseManagement.Communication;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import sensoremctrl.iotproject.paad.Model.DateAndTimeRepository;
+import sensoremctrl.iotproject.paad.Model.HumidityRepository;
+import sensoremctrl.iotproject.paad.Model.TemperatureRepository;
+
+@Component
+public class DatabaseLink {
+
+    @Autowired
+    TemperatureRepository temperatureRepository;
+
+    @Autowired
+    HumidityRepository humidityRepository;
+
+    @Autowired
+    DateAndTimeRepository dateAndTimeRepository;
+
+    @Autowired
+    DataTransfer dataTransfer;
+
+    @Bean
+    public boolean sendDataToDatabase() {
+        temperatureRepository.save(dataTransfer.storeTemperatureLog());
+        humidityRepository.save(dataTransfer.storeHumidityLog());
+        dateAndTimeRepository.save(dataTransfer.storeTimeStamp());
+        return true;
+    }
+}
