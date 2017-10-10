14133ad72b69bc4341bd71c71ec260ca3995f3f0
Patricio <patricio89morales5@hotmail.com>
2017-10-10 13:20:29 +0200

    minor fixes

--- src/main/java/sensoremctrl/iotproject/paad/DatabaseManagement/Communication/DataReceiver.java
+++ src/main/java/sensoremctrl/iotproject/paad/DatabaseManagement/Communication/DataReceiver.java
@@ -3,17 +3,20 @@ package sensoremctrl.iotproject.paad.DatabaseManagement.Communication;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.SupervisedData;
 import sensoremctrl.iotproject.paad.Model.SupervisedDataRepository;
+
+import java.util.List;
 
 @Component
 public class DataReceiver {
 
     @Autowired
     private SupervisedDataRepository repository;
-
     private String userEmail;
     private final int REQUESTED_HUMIDITY_CONTROLL = 33;
     private final int REQUESTED_TEMPERATURE_CONTROLL = 50;
+
 
 
     public String getUserEmail() {
@@ -22,7 +25,7 @@ public class DataReceiver {
     }
 
     public int getRequestedTemperatureValue() {
-        repository.findAll();
+
         return this.REQUESTED_HUMIDITY_CONTROLL;
     }
 
