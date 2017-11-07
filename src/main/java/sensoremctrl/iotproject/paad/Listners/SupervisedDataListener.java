77f0e9c2689ddd2fc9987533efe0da480bfcf2d4
Patricio <patricio89morales5@hotmail.com>
2017-11-07 10:59:00 +0100

    Csv Listener almost complete, need to change directory.

--- src/main/java/sensoremctrl/iotproject/paad/Listners/SupervisedDataListener.java
+++ src/main/java/sensoremctrl/iotproject/paad/Listners/SupervisedDataListener.java
@@ -1,8 +1,13 @@
 package sensoremctrl.iotproject.paad.Listners;
 
 import org.hibernate.EmptyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import sensoremctrl.iotproject.paad.DatabaseManagement.Entities.SupervisedData;
 
 public class SupervisedDataListener extends EmptyInterceptor{
+
+    @Autowired
+    SupervisedData supervisedData;
 
     public void onUpdate(){
 
